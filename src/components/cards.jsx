a4e966f34a524382f5850baf9d9016d9dbcfce1b
tenma1005 <sammy.andrade@outlook.fr>
2023-01-01 15:33:29 +0100

    modif (on fait un fetch)

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,25 +1,41 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-import LogementsData from "../data/logements.json";
+function Cards() {
+  const [cards, setCards] = useState();
 
-function Logements() {
+  useEffect(function () {
+    fetch("http://localhost:3000/logements.json")
+      .then(function (response) {
+        console.table(response);
+        return response.json();
+      })
+      .then(function (data) {
+        //console.log(data);
+        //console.table(data[0].id);
+        console.table(data);
+        setCards(data);
+      });
+  }, []);
+
   return (
     <section className="list-card">
-      {LogementsData.map(function (logement) {
-        return (
-          <article key={logement.id}>
-            <Link to={`/logements/${logement.id}`}>
-              <div className="card">
-                <img src={logement.cover} alt="" className="card-img" />
-                <h2 className="card-title">{logement.title}</h2>
-              </div>
-            </Link>
-          </article>
-        );
-      })}
+      {cards &&
+        cards.map(function (card) {
+          return (
+            <article key={card.id}>
+              <Link to={`/logements/${card.id}`}>
+                <div className="card">
+                  <img src={card.cover} alt="" className="card-img" />
+                  <h2 className="card-title">{card.title}</h2>
+                </div>
+              </Link>
+            </article>
+          );
+        })}
     </section>
   );
 }
 
-export default Logements;
+export default Cards;
