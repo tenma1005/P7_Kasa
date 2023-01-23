e9e5327cfa665cd03e9782c9aa9b388e302181c5
tenma1005 <sammy.andrade@outlook.fr>
2023-01-23 11:39:59 +0100

    navlink

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Cards({ logements }) {
   return (
@@ -9,12 +9,12 @@ function Cards({ logements }) {
         logements.map(function (logement) {
           return (
             <article key={logement.id}>
-              <Link to={`/logements/${logement.id}`}>
+              <NavLink to={`/logements/${logement.id}`}>
                 <div className="card">
                   <img src={logement.cover} alt="" className="card-img" />
                   <h2 className="card-title">{logement.title}</h2>
                 </div>
-              </Link>
+              </NavLink>
             </article>
           );
         })}
